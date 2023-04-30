4f38377affb4674a9ef2a74e3832eef469ed84f5
DevAkky <developerakky@gmail.com>
2023-04-30 14:33:51 +0530

    merging array

--- 5.merging.js
+++ 5.merging.js
@@ -1,2 +1,14 @@
 // MERGING TWO ARRAYS MANUALLY ---------------->>>
 
+let data = [3, 4, 5, 6, 7, 8];
+let data2 = [9, 10, 11, 12];
+let data3 = [];
+
+for (let i = 0; i < data.length; i++) {
+  data3[i] = data[i]; // 1st copy data into data3
+}
+for (let i = 0; i < data2.length; i++) {
+  data3[data.length + i] = data2[i]; // then copy data and data2 in data3
+}
+
+console.log(data3);
